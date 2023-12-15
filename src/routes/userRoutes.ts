e73b0ab4a4caee7131dc10606e8b34f39285724b
Felipe Severiano <felipe.kanner@gmail.com>
2023-12-15 11:52:00 -0300

    adjust Post User

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,7 +3,7 @@ import userController from '../controllers/userController';
 import validate from '../middlewares/validate';
 const router: Router = Router();
 
-router.post('/register', userController.register);
+router.post('/', userController.register);
 router.get('/', validate.auth, userController.findAll);
 router.get('/:id', validate.auth, userController.findById);
 router.put('/', validate.auth, userController.updateUser);
